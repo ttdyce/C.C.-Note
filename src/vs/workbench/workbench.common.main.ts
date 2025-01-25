a437db3acd867084f939e3a2f7306bbde929a3e6
ttdyce <ttdyce@gmail.com>
2025-01-25 23:29:17 +0800

    keep the extension contribution without registering view

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -265,8 +265,8 @@ import './contrib/customEditor/browser/customEditor.contribution.js';
 import './contrib/externalUriOpener/common/externalUriOpener.contribution.js';
 
 // Extensions Management
-// import './contrib/extensions/browser/extensions.contribution.js';
-// import './contrib/extensions/browser/extensionsViewlet.js';
+import './contrib/extensions/browser/extensions.contribution.js';
+import './contrib/extensions/browser/extensionsViewlet.js';
 
 // Output View
 import './contrib/output/browser/output.contribution.js';
